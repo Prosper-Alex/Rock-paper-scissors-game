c31cda83e04d44309dcfbe427d406e473508c147
Prosper Alex <chigoziealex011@gmail.com>
2025-06-28 09:29:39 +0100

    Add element references for Rock-Paper-Scissors game

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const message = document.getElementById("message");
+const scoreBoard = document.getElementById("score-borad");
